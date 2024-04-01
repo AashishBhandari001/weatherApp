f5f3ca3cefb556fcc8b6d751ea5830107943ccb6
Aashish <np03cs4m210054@heraldcollege.edu.np>
2024-04-01 16:55:08 +0545

    Weather app using plain html and Javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+const timeE1 = document.getElementById('time');
+const dateE1 = document.getElementById("date");
+const currentWeatherItemsE1 = document.getElementById("current-weather-items");
+const timezone = document.getElementById("time-zone");
+const counrtyE1 = document.getElementById("country");
+const weatherforecastE1 = document.getElementById("weather-forecast");
+const currentTemp1 = document.getElementById("current-temp");
+
+set
